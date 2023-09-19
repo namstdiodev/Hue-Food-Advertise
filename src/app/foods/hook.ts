a71f9a59b77f6134b76e8d56280614ec5cb3fa8d
chaunguyennam.dev <chaunguyennam.dev@gmail.com>
2023-09-19 11:06:22 +0700

    [feature] intergrate api for create food and list food

--- src/app/foods/hook.ts
+++ src/app/foods/hook.ts
@@ -1,8 +1,23 @@
 import { ReceivedProps } from "./type";
+import { db } from "../../../firebase/initFirebase";
+import { collection, getDoc, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 const useFoods = (props: ReceivedProps) => {
+  const [foods, setFoods] = useState([]);
+  const fetchFoods = async () => {
+    try {
+      const response: any = await getDocs(collection(db, "foods"));
+      setFoods(response.docs.map((doc: any) => ({ ...doc.data(), id: doc.id })));
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchFoods();
+  }, []);
   return {
     ...props,
+    foods,
   };
 };
 
